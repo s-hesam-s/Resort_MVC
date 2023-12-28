aec61233d9238a3a44a2a32244bd707d2f7d57ba
s-hesam-s <hesamshoushtarian@gmail.com>
2023-12-28 22:00:02 +0330

    Section 3 - Create Villa Controller and Retrieve All Villa

---
+++ Resort.Web/Controllers/VillaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resort.Domain.Entities;
+using Resort.Infrastructure.Data;
+
+namespace Resort.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Villa> villas = _db.Villas.ToList();
+            
+            return View();
+        }
+    }
+}

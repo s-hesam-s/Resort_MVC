54fb16e71cda37ca0342e2a1afbef983b1d117ef
s-hesam-s <hesamshoushtarian@gmail.com>
2023-12-29 01:01:11 +0330

    Section 3 - More Data Annotations

--- Resort.Domain/Entities/Villa.cs
+++ Resort.Domain/Entities/Villa.cs
@@ -10,15 +10,26 @@ namespace Resort.Domain.Entities
     public class Villa
     {
         public int Id { get; set; }
+
+        [MaxLength(50)]
         public required string Name { get; set; }
+
         public string? Description { get; set; }
+
         [Display(Name = "Price Per Night")]
+        [Range(10, 10000)]
         public double Price { get; set; }
+
         public int Sqft { get; set; }
+
+        [Range(1, 10)]
         public int Occupancy { get; set; }
+
         [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
+
         public DateTime? CreatedDate { get; set; }
+
         public DateTime? UpdatedDate { get; set; }
     }
 }

881f4bb9137e1e584b853724d9102d991b05d5c7
s-hesam-s <hesamshoushtarian@gmail.com>
2023-12-29 00:51:12 +0330

    Section 3 - Display Data Annotation

--- Resort.Domain/Entities/Villa.cs
+++ Resort.Domain/Entities/Villa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,11 @@ namespace Resort.Domain.Entities
         public int Id { get; set; }
         public required string Name { get; set; }
         public string? Description { get; set; }
+        [Display(Name = "Price Per Night")]
         public double Price { get; set; }
         public int Sqft { get; set; }
         public int Occupancy { get; set; }
+        [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }

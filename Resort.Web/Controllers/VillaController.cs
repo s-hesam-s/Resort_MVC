92d5ce4d0c1cff2cb1bd4fe7897aeb2d7641bb82
s-hesam-s <hesamshoushtarian@gmail.com>
2023-12-29 01:06:52 +0330

    Section 3 - Custom Model Validations

--- Resort.Web/Controllers/VillaController.cs
+++ Resort.Web/Controllers/VillaController.cs
@@ -28,6 +28,11 @@ namespace Resort.Web.Controllers
         [HttpPost]
         public IActionResult Create(Villa obj)
         {
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("name", "The description cannot exactly match the Name.");
+            }
+
             if (ModelState.IsValid)
             {
                 _db.Villas.Add(obj);

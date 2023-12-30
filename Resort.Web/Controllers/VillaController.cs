64f184594dbfd54c98121195dbd8ffdaf5b5121a
s-hesam-s <hesamshoushtarian@gmail.com>
2023-12-30 22:25:11 +0330

    Section 4 - Update in Action

--- Resort.Web/Controllers/VillaController.cs
+++ Resort.Web/Controllers/VillaController.cs
@@ -51,5 +51,17 @@ namespace Resort.Web.Controllers
             }
             return View(obj);
         }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+                _db.Villas.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }

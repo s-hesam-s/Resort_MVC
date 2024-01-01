f387de3bf6e6dd8b7950b0012590ebdb628fc549
s-hesam-s <hesamshoushtarian@gmail.com>
2024-01-01 19:44:05 +0330

    Section 7 - VillaRepository Interface

---
+++ Resort.Application/Common/Interfaces/IVillaRepository.cs
@@ -0,0 +1,20 @@
+﻿using Resort.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resort.Application.Common.Interfaces
+{
+    public interface IVillaRepository
+    {
+        IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null);
+        IEnumerable<Villa> Get(Expression<Func<Villa, bool>> filter, string? includeProperties = null);
+        void Add(Villa entity);
+        void Update(Villa entity);
+        void Remove(Villa entity);
+        void Save();
+    }
+}

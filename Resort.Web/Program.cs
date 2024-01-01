b381292df1516e9f33b8ddc17019c12987336c59
s-hesam-s <hesamshoushtarian@gmail.com>
2024-01-01 19:49:27 +0330

    Section 7 - Implement Villa Repository

--- Resort.Web/Program.cs
+++ Resort.Web/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Resort.Application.Common.Interfaces;
 using Resort.Infrastructure.Data;
+using Resort.Infrastructure.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
           options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IVillaRepository, VillaRepository>();
 
 var app = builder.Build();
 

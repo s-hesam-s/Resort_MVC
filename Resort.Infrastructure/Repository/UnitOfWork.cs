f677eb99910218ea5a8e8744d158502795590ecb
s-hesam-s <hesamshoushtarian@gmail.com>
2024-01-01 23:05:21 +0330

    Section 7 - UnitOfWork Implementation

---
+++ Resort.Infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using Resort.Application.Common.Interfaces;
+using Resort.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resort.Infrastructure.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public IVillaRepository Villa { get; private set; }
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Villa = new VillaRepository(_db);
+        }
+    }
+}

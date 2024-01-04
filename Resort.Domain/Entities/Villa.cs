c52690417bb62f2d1eedfffebec316d9e8601f1d
s-hesam-s <hesamshoushtarian@gmail.com>
2024-01-04 19:08:07 +0330

    Section 8 - Add Form File in Villa Model

--- Resort.Domain/Entities/Villa.cs
+++ Resort.Domain/Entities/Villa.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +26,9 @@ namespace Resort.Domain.Entities
 
         [Range(1, 10)]
         public int Occupancy { get; set; }
+
+        [NotMapped]
+        public IFormFile? Image { get; set; }
 
         [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }

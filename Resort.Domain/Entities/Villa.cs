64911afb0f8554d55c57283464d29a1171501dc1
s-hesam-s <hesamshoushtarian@gmail.com>
2024-01-04 23:26:29 +0330

    Section 10 - Load Villa Amenity

--- Resort.Domain/Entities/Villa.cs
+++ Resort.Domain/Entities/Villa.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -36,5 +37,8 @@ namespace Resort.Domain.Entities
         public DateTime? CreatedDate { get; set; }
 
         public DateTime? UpdatedDate { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<Amenity> VillaAmenity { get; set; }
     }
 }

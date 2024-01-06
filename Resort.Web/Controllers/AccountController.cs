0025d9f2ef268b02a798cd771410117282ffb21a
s-hesam-s <hesamshoushtarian@gmail.com>
2024-01-07 00:39:35 +0330

    Section 11 - Register Post Endpoint

--- Resort.Web/Controllers/AccountController.cs
+++ Resort.Web/Controllers/AccountController.cs
@@ -55,5 +55,59 @@ namespace Resort.Web.Controllers
 
             return View(registerVM);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            ApplicationUser user = new()
+            {
+                Name = registerVM.Name,
+                Email = registerVM.Email,
+                PhoneNumber = registerVM.PhoneNumber,
+                NormalizedEmail = registerVM.Email.ToUpper(),
+                EmailConfirmed = true,
+                UserName = registerVM.Email,
+                CreatedAt = DateTime.Now
+            };
+
+            var result = await _userManager.CreateAsync(user, registerVM.Password);
+
+            if (result.Succeeded)
+            {
+                if (!string.IsNullOrEmpty(registerVM.Role))
+                {
+                    await _userManager.AddToRoleAsync(user, registerVM.Role);
+                }
+                else
+                {
+                    await _userManager.AddToRoleAsync(user, SD.Role_Customer);
+                }
+
+                await _signInManager.SignInAsync(user, isPersistent: false);
+
+                if (!string.IsNullOrEmpty(registerVM.RedirectUrl))
+                {
+                    return LocalRedirect(registerVM.RedirectUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                registerVM.RoleList = _roleManager.Roles.Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Name
+                });
+
+                return View(registerVM);
+            }
+        }
     }
 }

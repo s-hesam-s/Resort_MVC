9e05986a615e85b678242aa07f5159aef21df60e
s-hesam-s <hesamshoushtarian@gmail.com>
2024-01-06 22:20:07 +0330

    Section 11 - Add Account Controller and Inject Helper Dependencies

---
+++ Resort.Web/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Resort.Application.Common.Interfaces;
+using Resort.Domain.Entities;
+
+namespace Resort.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(IUnitOfWork unitOfWork,
+               UserManager<ApplicationUser> userManager,
+               RoleManager<IdentityRole> roleManager,
+               SignInManager<ApplicationUser> signInManager)
+        {
+            _roleManager = roleManager;
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}

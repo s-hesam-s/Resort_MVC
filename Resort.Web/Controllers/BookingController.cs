7614f6b252b957fce8d783246f1b56b4758da1a6
s-hesam-s <hesamshoushtarian@gmail.com>
2024-01-09 22:50:15 +0330

    Section 13 - Get Action Finalize Booking

---
+++ Resort.Web/Controllers/BookingController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resort.Application.Common.Interfaces;
+using Resort.Domain.Entities;
+
+namespace Resort.Web.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public BookingController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult FinalizeBooking(int villaId, DateOnly checkInDate, int nights)
+        {
+            Booking booking = new()
+            {
+                VillaId = villaId,
+                Villa = _unitOfWork.Villa.Get(u => u.Id == villaId, includeProperties: "VillaAmenity"),
+                CheckInDate = checkInDate,
+                Nights = nights,
+                CheckOutDate = checkInDate.AddDays(nights),
+            };
+
+            return View(booking);
+        }
+    }
+}

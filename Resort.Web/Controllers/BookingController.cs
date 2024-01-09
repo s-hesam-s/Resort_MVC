5b4152e7ea5dea73b468d2f841ffebc46d21708b
s-hesam-s <hesamshoushtarian@gmail.com>
2024-01-10 00:28:13 +0330

    Section 13 - Finalize Booking Dynamic UI

--- Resort.Web/Controllers/BookingController.cs
+++ Resort.Web/Controllers/BookingController.cs
@@ -22,6 +22,7 @@ namespace Resort.Web.Controllers
                 Nights = nights,
                 CheckOutDate = checkInDate.AddDays(nights),
             };
+            booking.TotalCost = booking.Villa.Price * nights;
 
             return View(booking);
         }

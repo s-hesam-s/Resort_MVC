1687b5ae45ef98efa6a046c64b8015de04294fa1
s-hesam-s <hesamshoushtarian@gmail.com>
2024-01-11 17:37:19 +0330

    Section 14 - Booking Details Action Method

--- Resort.Web/Controllers/BookingController.cs
+++ Resort.Web/Controllers/BookingController.cs
@@ -119,6 +119,15 @@ namespace Resort.Web.Controllers
             return View(bookingId);
         }
 
+        [Authorize]
+        public IActionResult BookingDetails(int bookingId)
+        {
+            Booking bookingFromDb = _unitOfWork.Booking.Get(u => u.Id == bookingId,
+             includeProperties: "User,Villa");
+
+            return View(bookingFromDb);
+        }
+
 
         #region API Calls
         [HttpGet]

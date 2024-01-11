17d3f485cc3ad3bf69bac5a1835a76817b792a5f
s-hesam-s <hesamshoushtarian@gmail.com>
2024-01-11 17:13:39 +0330

    Section 14 - DataTables

--- Resort.Web/Controllers/BookingController.cs
+++ Resort.Web/Controllers/BookingController.cs
@@ -16,6 +16,7 @@ namespace Resort.Web.Controllers
             _unitOfWork = unitOfWork;
         }
 
+        [Authorize]
         public IActionResult Index()
         {
             return View();
---
+++ Resort.Web/wwwroot/js/booking.js
@@ -0,0 +1,34 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+
+});
+
+function loadDataTable() {
+    dataTable = $('#tblBookings').DataTable({
+        "ajax": {
+            url: '/booking/getall'
+        },
+        "columns": [
+            { data: 'id', "width": "5%" },
+            { data: 'name', "width": "15%" },
+            { data: 'phone', "width": "10%" },
+            { data: 'email', "width": "15%" },
+            { data: 'status', "width": "10%" },
+            { data: 'checkInDate', "width": "10%" },
+            { data: 'nights', "width": "10%" },
+            { data: 'totalCost', render: $.fn.dataTable.render.number(',', '.', 2), "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group">
+                        <a href="/booking/bookingDetails?bookingId=${data}" class="btn btn-outline-warning mx-2">
+                            <i class="bi bi-pencil-square"></i> Details
+                        </a>
+                    </div>`
+                }
+            }
+        ]
+    });
+}

0f4f77a6386ad8d83a70f8d115645e3095b9d8f8
s-hesam-s <hesamshoushtarian@gmail.com>
2024-01-11 16:55:35 +0330

    Section 14 - Endpoint for DataTable

--- Resort.Web/Controllers/BookingController.cs
+++ Resort.Web/Controllers/BookingController.cs
@@ -117,5 +117,30 @@ namespace Resort.Web.Controllers
 
             return View(bookingId);
         }
+
+
+        #region API Calls
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetAll()
+        {
+            IEnumerable<Booking> objBookings;
+
+            if (User.IsInRole(SD.Role_Admin))
+            {
+                objBookings = _unitOfWork.Booking.GetAll(includeProperties: "User,Villa");
+            }
+            else
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                objBookings = _unitOfWork.Booking
+                    .GetAll(u => u.UserId == userId, includeProperties: "User,Villa");
+            }
+            return Json(new { data = objBookings });
+        }
+
+        #endregion
     }
 }

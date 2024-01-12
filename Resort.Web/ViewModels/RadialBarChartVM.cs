5fafe90e69b0888f9fecc255359890cb667bb462
s-hesam-s <hesamshoushtarian@gmail.com>
2024-01-12 18:08:44 +0330

    Section 16 - RadialBarChart VM

---
+++ Resort.Web/ViewModels/RadialBarChartVM.cs
@@ -0,0 +1,10 @@
+﻿namespace Resort.Web.ViewModels
+{
+    public class RadialBarChartVM
+    {
+        public decimal TotalCount { get; set; }
+        public decimal IncreaseDecreaseAmount { get; set; }
+        public bool HasRatioIncreased { get; set; }
+        public int[] Series { get; set; }
+    }
+}

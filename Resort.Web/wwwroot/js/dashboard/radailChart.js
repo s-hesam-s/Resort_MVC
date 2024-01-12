ef93665bddb84dbd980b7e1664ba224f455e7920
s-hesam-s <hesamshoushtarian@gmail.com>
2024-01-12 19:30:39 +0330

    Section 16 - Custom Color for Radial Chart

--- Resort.Web/wwwroot/js/dashboard/radailChart.js
+++ Resort.Web/wwwroot/js/dashboard/radailChart.js
@@ -1,5 +1,9 @@
 ﻿function loadRadialBarChart(id, data) {
+    var chartColors = getChartColorsArray(id);
     var options = {
+        fill: {
+            colors: chartColors
+        },
         chart: {
             height: 90,
             width: 90,
@@ -15,6 +19,7 @@
                 dataLabels: {
                     value: {
                         offsetY: -10,
+                        color: chartColors[0],
                     }
                 }
             }
@@ -24,4 +29,22 @@
     var chart = new ApexCharts(document.querySelector("#" + id), options);
     chart.render();
 
+}
+
+
+function getChartColorsArray(id) {
+    if (document.getElementById(id) !== null) {
+        var colors = document.getElementById(id).getAttribute("data-colors");
+        if (colors) {
+            colors = JSON.parse(colors);
+            return colors.map(function (value) {
+                var newValue = value.replace(" ", "");
+                if (newValue.indexOf(",") === -1) {
+                    var color = getComputedStyle(document.documentElement).getPropertyValue(newValue);
+                    if (color) return color;
+                    else return newValue;
+                }
+            });
+        }
+    }
 }

d23d59fc5a09f6c8903d87f6e72d07af2cf1ad2d
s-hesam-s <hesamshoushtarian@gmail.com>
2024-01-18 22:09:20 +0330

    Section 17 - Download PPT

--- Resort.Web/Controllers/HomeController.cs
+++ Resort.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Resort.Application.Common.Interfaces;
 using Resort.Application.Common.Utility;
 using Resort.Web.Models;
 using Resort.Web.ViewModels;
+using Syncfusion.Presentation;
 using System.Diagnostics;
 
 namespace Resort.Web.Controllers
@@ -11,9 +12,12 @@ namespace Resort.Web.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -49,6 +53,99 @@ namespace Resort.Web.Controllers
             };
 
             return PartialView("_VillaList", homeVM);
+        }
+
+        [HttpPost]
+        public IActionResult GeneratePPTExport(int id)
+        {
+            var villa = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity").FirstOrDefault(x => x.Id == id);
+            if (villa is null)
+            {
+                return RedirectToAction(nameof(Error));
+            }
+
+            string basePath = _webHostEnvironment.WebRootPath;
+            string filePath = basePath + @"/Exports/ExportVillaDetails.pptx";
+
+            using IPresentation presentation = Presentation.Open(filePath);
+
+            ISlide slide = presentation.Slides[0];
+
+            IShape? shape = slide.Shapes.FirstOrDefault(u => u.ShapeName == "txtVillaName") as IShape;
+            if (shape is not null)
+            {
+                shape.TextBody.Text = villa.Name;
+            }
+
+            shape = slide.Shapes.FirstOrDefault(u => u.ShapeName == "txtVillaDescription") as IShape;
+            if (shape is not null)
+            {
+                shape.TextBody.Text = villa.Description;
+            }
+
+            shape = slide.Shapes.FirstOrDefault(u => u.ShapeName == "txtOccupancy") as IShape;
+            if (shape is not null)
+            {
+                shape.TextBody.Text = string.Format("Max Occupancy : {0} adults", villa.Occupancy);
+            }
+
+            shape = slide.Shapes.FirstOrDefault(u => u.ShapeName == "txtVillaSize") as IShape;
+            if (shape is not null)
+            {
+                shape.TextBody.Text = string.Format("Villa Size: {0} sqft", villa.Sqft);
+            }
+
+            shape = slide.Shapes.FirstOrDefault(u => u.ShapeName == "txtPricePerNight") as IShape;
+            if (shape is not null)
+            {
+                shape.TextBody.Text = string.Format("USD {0}/night", villa.Price.ToString("C"));
+            }
+
+            shape = slide.Shapes.FirstOrDefault(u => u.ShapeName == "txtVillaAmenitiesHeading") as IShape;
+            if (shape is not null)
+            {
+                List<string> listItems = villa.VillaAmenity.Select(x => x.Name).ToList();
+
+                shape.TextBody.Text = "";
+
+                foreach (var item in listItems)
+                {
+                    IParagraph paragraph = shape.TextBody.AddParagraph();
+                    ITextPart textPart = paragraph.AddTextPart(item);
+
+                    paragraph.ListFormat.Type = ListType.Bulleted;
+                    paragraph.ListFormat.BulletCharacter = '\u2022';
+                    textPart.Font.FontName = "system-ui";
+                    textPart.Font.FontSize = 18;
+                    textPart.Font.Color = ColorObject.FromArgb(144, 148, 152);
+                }
+
+            }
+
+            shape = slide.Shapes.FirstOrDefault(u => u.ShapeName == "imgVilla") as IShape;
+            if (shape is not null)
+            {
+                byte[] imageData;
+                string imageUrl;
+                try
+                {
+                    imageUrl = string.Format("{0}{1}", basePath, villa.ImageUrl);
+                    imageData = System.IO.File.ReadAllBytes(imageUrl);
+                }
+                catch (Exception)
+                {
+                    imageUrl = string.Format("{0}{1}", basePath, "/images/placeholder.png");
+                    imageData = System.IO.File.ReadAllBytes(imageUrl);
+                }
+                slide.Shapes.Remove(shape);
+                using MemoryStream imageStream = new(imageData);
+                IPicture newPicture = slide.Pictures.AddPicture(imageStream, 60, 120, 300, 200);
+            }
+
+            MemoryStream memoryStream = new();
+            presentation.Save(memoryStream);
+            memoryStream.Position = 0;
+            return File(memoryStream, "application/pptx", "villa.pptx");
         }
 
 

3379533939bc1e7bc858fa473245568295ec45f3
s-hesam-s <hesamshoushtarian@gmail.com>
2024-01-19 23:18:18 +0330

    Section 20 - Booking Service

---
+++ Resort.Application/Services/Interface/IBookingService.cs
@@ -0,0 +1,16 @@
+﻿using Resort.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resort.Application.Services.Interface
+{
+    public interface IBookingService
+    {
+        void CreateBooking(Booking booking);
+        Booking GetBookingById(int bookingId);
+        IEnumerable<Booking> GetAllBookings(string userId = "", string? statusFilterList = "");
+    }
+}

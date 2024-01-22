4df1d890f5a736776a8704980b77767e5fcd093f
s-hesam-s <hesamshoushtarian@gmail.com>
2024-01-22 22:36:01 +0330

    Section 20 - Fix Booking Controller

--- Resort.Application/Services/Interface/IBookingService.cs
+++ Resort.Application/Services/Interface/IBookingService.cs
@@ -12,7 +12,10 @@ namespace Resort.Application.Services.Interface
         void CreateBooking(Booking booking);
         Booking GetBookingById(int bookingId);
         IEnumerable<Booking> GetAllBookings(string userId = "", string? statusFilterList = "");
+
         void UpdateStatus(int bookingId, string bookingStatus, int villaNumber);
         void UpdateStripePaymentID(int bookingId, string sessionId, string paymentIntentId);
+
+        public IEnumerable<int> GetCheckedInVillaNumbers(int villaId);
     }
 }
